de26a36f6333a23d44af3ba75655d89b65232cf3
guoxiujuan <836832555@qq.com>
2019-04-29 13:49:21 +0800

    5811

---
+++ www/js/lib.js
@@ -0,0 +1,27 @@
+﻿
+$(document).ready(function($) {
+	
+	// 手机导航
+	$('.menuBtn').append('<b></b><b></b><b></b>');
+	$('.menuBtn').click(function(event) {
+		$(this).toggleClass('open');
+		var _winw = $(window).width();
+		var _winh = $(window).height();
+		if( $(this).hasClass('open') ){
+			$('body').addClass('open');
+			if( _winw<=992 ){
+				$('.soBox').stop().slideDown();
+				$('#nv').stop().slideDown();
+			}
+		}else{
+			$('body').removeClass('open');
+			if( _winw<=992 ){
+				$('.soBox').stop().slideUp();
+				$('#nv').stop().slideUp();
+			}
+		}
+	});
+
+	window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"32"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
+	
+});
